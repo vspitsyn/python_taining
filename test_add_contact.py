4b84bb484e559b8cb1bad2935324d80d6a601f52
vspitsyn <spitsyn_v@mail.ru>
2017-08-07 17:40:31 +0300

    переведены тесты с unittest на py.test и реализованы фикстуры

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,115 +1,48 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-
-#загружаем модуль для работы с селектами
-from selenium.webdriver.support.ui import Select
-
 from contact import Contact
-
-import time, unittest
-
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd =  WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+from application import Application
+import pytest
 
 
-    def login(self, username, password):
-        wd = self.wd
-        wd.get("http://localhost:8080/addressbook")
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def create_contact(self, contact):
-        wd = self.wd
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
+def test_new_contact(app):
+    # login
+    app.login("admin", "secret")
 
-        # fill contact form
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # wd.find_element_by_name("photo").click()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").send_keys(contact.company_address)
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").send_keys(contact.email1)
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-            # Birthday date
-        selectDay1 = Select(wd.find_element_by_xpath("//select[@name='bday']"))
-        selectDay1.select_by_visible_text(contact.birth_day)
-        selectMon1 = Select(wd.find_element_by_xpath("//select[@name='bmonth']"))
-        selectMon1.select_by_visible_text(contact.birth_month)
-        wd.find_element_by_name("byear").send_keys(contact.birth_year)
-            # Anniversary date
-        selectDay1 = Select(wd.find_element_by_xpath("//select[@name='aday']"))
-        selectDay1.select_by_visible_text(contact.anniver_day)
-        selectMon1 = Select(wd.find_element_by_xpath("//select[@name='amonth']"))
-        selectMon1.select_by_visible_text(contact.anniver_month)
-        wd.find_element_by_name("ayear").send_keys(contact.anniver_year)
+    #create contact object
+    contact1 = Contact(firstname  = "Elena",
+                        middlename = "Petrovna",
+                        lastname = "Ivanova",
+                        nickname = "EPI",
+                        title = "Secretary",
+                        company = "Sviaz-Bank",
+                        company_address = "St. Novoryazanskaya, d. 31/7, korp.1, Moscow",
+                        home_phone = "+77776665544",
+                        mobile_phone = "+78883332244",
+                        work_phone = "+71112223344",
+                        fax = "no",
+                        email1 = "epi100@mail.ru",
+                        email2 = "no",
+                        email3 = "no",
+                        homepage = "www.epi100.ru",
+                        birth_day = "3",
+                        birth_month = "February",
+                        birth_year = "1986",
+                        anniver_day = "3",
+                        anniver_month  ="February",
+                        anniver_year = "2006",
+                        home_address = "St.16 Parkovaya, D. 4, kV. 1, Moscow",
+                        home_phone2 = "no",
+                        notes = "two children")
+    #create contact
+    app.create_contact(contact1)
 
-        wd.find_element_by_name("address2").send_keys(contact.home_address)
-        wd.find_element_by_name("phone2").send_keys(contact.home_phone2)
-        wd.find_element_by_name("notes").send_keys(contact.notes)
+    # logout
+    app.logout()
 
-        # submit contact creation
-        wd.find_element_by_xpath("//input[@value='Enter']").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_new_contact(self):
-        # login
-        self.login("admin", "secret")
-
-        #create contact object
-        contact1 = Contact(firstname  = "Elena",
-                          middlename = "Petrovna",
-                          lastname = "Ivanova",
-                          nickname = "EPI",
-                          title = "Secretary",
-                          company = "Sviaz-Bank",
-                          company_address = "St. Novoryazanskaya, d. 31/7, korp.1, Moscow",
-                          home_phone = "+77776665544",
-                          mobile_phone = "+78883332244",
-                          work_phone = "+71112223344",
-                          fax = "no",
-                          email1 = "epi100@mail.ru",
-                          email2 = "no",
-                          email3 = "no",
-                          homepage = "www.epi100.ru",
-                          birth_day = "3",
-                          birth_month = "February",
-                          birth_year = "1986",
-                          anniver_day = "3",
-                          anniver_month  ="February",
-                          anniver_year = "2006",
-                          home_address = "St.16 Parkovaya, D. 4, kV. 1, Moscow",
-                          home_phone2 = "no",
-                          notes = "two children")
-        #create contact
-        self.create_contact(contact1)
-        # logout
-        self.logout()
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
--- test_add_group.py
+++ test_add_group.py
@@ -1,92 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-#from selenium.webdriver.common.action_chains import ActionChains
 from group import Group
+from application import Application
+import pytest
+#import time, unittest
 
 
-import time, unittest
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+def test_add_group(app):
+    app.login(username="admin", password="secre")
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="ИУ6", header="We", footer="Are"))
+    app.logout()
 
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost:8080/addressbook/")
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-
-    def open_groups_page(self):
-        wd = self.wd
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-
-    def test_add_group(self):
-        self.login(username="admin", password="secre")
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="ИУ6", header="We", footer="Are"))
-        self.logout()
-
-    def test_add__empty_group(self):
-        #wd = self.wd
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
