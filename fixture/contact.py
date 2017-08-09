d00fa8139805719c4971634fd468b06564cc8d24
vspitsyn <spitsyn_v@mail.ru>
2017-08-09 16:35:39 +0300

    реализован тест редактирования контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -56,3 +56,67 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
         time.sleep(3)
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        # select_first_contact
+        if not wd.find_element_by_name("selected[]").is_selected():
+            wd.find_element_by_name("selected[]").click()
+        # init contact deletion
+        #wd.find_element_by_xpath("//input[@title='Edit']").click()
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        # wd.find_element_by_name("photo").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.company_address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email1)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        # Birthday date
+        selectDay1 = Select(wd.find_element_by_xpath("//select[@name='bday']"))
+        selectDay1.select_by_visible_text(contact.birth_day)
+        selectMon1 = Select(wd.find_element_by_xpath("//select[@name='bmonth']"))
+        selectMon1.select_by_visible_text(contact.birth_month)
+        wd.find_element_by_name("byear").send_keys(contact.birth_year)
+        # Anniversary date
+        selectDay1 = Select(wd.find_element_by_xpath("//select[@name='aday']"))
+        selectDay1.select_by_visible_text(contact.anniver_day)
+        selectMon1 = Select(wd.find_element_by_xpath("//select[@name='amonth']"))
+        selectMon1.select_by_visible_text(contact.anniver_month)
+        wd.find_element_by_name("ayear").send_keys(contact.anniver_year)
+
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.home_address)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.home_phone2)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+
+        # submit contact edition
+        wd.find_element_by_xpath("//input[@value='Update']").click()
+        time.sleep(3)
---
+++ test/test_edit_contact.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+def test_edit_first_contact(app):
+    # login
+    app.session.login("admin", "secret")
+
+    #create contact object
+    contact1 = Contact(firstname  = "Elena",
+                        middlename = "Ivanovna",
+                        lastname = "Petrova",
+                        nickname = "EPI",
+                        title = "Secretary",
+                        company = "Sviaz-Bank",
+                        company_address = "St. Novoryazanskaya, d. 31/7, korp.1, Moscow",
+                        home_phone = "+77776665544",
+                        mobile_phone = "+78883332244",
+                        work_phone = "+71112223344",
+                        fax = "no",
+                        email1 = "epi100@mail.ru",
+                        email2 = "no",
+                        email3 = "no",
+                        homepage = "www.epi100.ru",
+                        birth_day = "4",
+                        birth_month = "February",
+                        birth_year = "1986",
+                        anniver_day = "4",
+                        anniver_month  ="February",
+                        anniver_year = "2006",
+                        home_address = "St.15 Parkovaya, D. 4, kV. 1, Moscow",
+                        home_phone2 = "no",
+                        notes = "two children")
+    #create contact
+    app.contact.edit_first_contact(contact1)
+
+    # logout
+    app.session.logout()
