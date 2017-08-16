5003f68d0b241e1fc1701ac75c9800dc89189478
vspitsyn <spitsyn_v@mail.ru>
2017-08-16 17:33:55 +0300

    оптимизированы переходы между страницами
    если находимся на странице, то на нее не переходим, а остаемся на ней.

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add"))>0):
+            wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
         wd = self.app.wd
