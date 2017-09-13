198093baa59aa9dbc79432ca72342f8a303458c2
vspitsyn <spitsyn_v@mail.ru>
2017-09-13 17:43:13 +0300

    создана фикстура для установки соединения с БД
    усложнена структура конфигурационного файла, считывание конфигурационного файла вынесено в отдельную функцию.

---
+++ check_db_connection.py
@@ -0,0 +1,14 @@
+#import mysql.connector
+import pymysql.cursors
+
+#connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+finally:
+    connection.close()
+
